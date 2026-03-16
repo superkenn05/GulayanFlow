1afe6717059240e66e4fc9d1c8b268be81b7e80c
Mark Ken Madrid <markkenmadrid05@gmail.com>
2026-03-16 07:28:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8837636834-85102",
+  "appId": "1:165495298092:web:a2af86cfd1502f5241d989",
+  "apiKey": "AIzaSyC4WPVA6tvaFc1pnc2JRp3OTD3GLIW77-0",
+  "authDomain": "studio-8837636834-85102.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "165495298092"
+};
