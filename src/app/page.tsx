af76a7e3630447f494a0f752ebe009c05ab8f62c
Mark Ken Madrid <markkenmadrid05@gmail.com>
2026-03-18 13:49:14 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Missing or insuffi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,16 @@
 import React, { useEffect, useState } from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-import { Package, AlertTriangle, ArrowUpRight, ArrowDownRight, TrendingUp, ShoppingBag, Loader2, ShoppingBasket } from "lucide-react"
+import { 
+  Package, 
+  AlertTriangle, 
+  ArrowUpRight, 
+  ArrowDownRight, 
+  TrendingUp, 
+  ShoppingBag, 
+  Loader2, 
+  ShoppingBasket 
+} from "lucide-react"
 import {
   BarChart,
   Bar,
