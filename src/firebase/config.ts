5f6d63117c924ecf45530a939ea0bf1e3c2ce69c
Mark Ken Madrid <markkenmadrid05@gmail.com>
2026-03-20 11:20:58 +0000

    Whot to fix this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8837636834-85102",
-  "appId": "1:165495298092:web:a2af86cfd1502f5241d989",
-  "apiKey": "AIzaSyC4WPVA6tvaFc1pnc2JRp3OTD3GLIW77-0",
-  "authDomain": "studio-8837636834-85102.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "165495298092"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
 };
