302b8ee98510ebc44477c95cd2783b5a34bdfa29
superkenn05 <markkenmadrid05@gmail.com>
2026-04-07 09:54:48 +0800

    Update Firebase config and hosting setup

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "gemma-s-gulayan",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:124342894779:web:be9e98c9c3993b5f7f699e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCbEgSXCCaUJW8tIj2RYvwoJp1HI8_R5To",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "gemma-s-gulayan.firebaseapp.com",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "gemma-s-gulayan.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "124342894779",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-C7VB7HJNQZ",
 };
